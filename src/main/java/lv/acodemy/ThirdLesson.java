3c00d8bdb1ce1359022704f9b0ec5f76fc5981f7
Diana Spiridonova <dspiridonova604@gmail.com>
2023-11-09 15:43:49 +0200

    [master] : Upload learning project

--- src/main/java/lv/acodemy/ThirdLesson.java
+++ src/main/java/lv/acodemy/ThirdLesson.java
@@ -1,99 +1,96 @@
 package lv.acodemy;
 
 public class ThirdLesson {
-    for(
-    int i = 0;
-    i< 10;i++);
-    int i = 1;
+    public static void main(String[] args) {
 
-    for(
-    int i = 10;
-    i <=5;i++)
-    String[] arrayOfNames = {"Nikita", "John", "Mike", "Anthony", "Mary", "Ann"};
-
-    for(
-    int i = 0;
-    i >=0;i--)
-    String[] countries = {
-            "United States",
-            "Canada",
-            "United Kingdom",
-            "Germany",
-            "France",
-            "Australia",
-            "Japan",
-            "India",
-            "China",
-            "Brazil"
-    };
-    int i = 0;
-
-
-    for(
-    int z = 1;
-    i <=arrayOfNames.lenght;i++)
-
-    {
-        System.out.println("Hello World!");
-    }
-
-    {
-        System.out.println(i);
-    }
-
-    // Goal: Find Japan in our array!
-
-    for(
-
-    {
-        System.out.println(i);
-    }
-    i<countries.length;i++)
-
-    {
-        System.out.println(arrayOfNames[i]);
-
-    }
-
-    {
-        if (countries[i].equals("Japan")) {
-            System.out.println("JAMETE KUDASAI!");
-//            break;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World!");
         }
 
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
 
-        int[] firstArray = new int[0];
-        for (int numberOfFirstArray : firstArray) {
-            int[] secondArray = new int[0];
-            for (int numberOfSecondArray : secondArray) {
-                int[] thirdArray = new int[0];
-                for (int numberOfThirdArray : thirdArray) {
+        for (int i = 10; i >= 0; i--) {
+            System.out.println(i);
+        }
 
-                    firstArray = {1, 2, 3, 4, 5};
-                    secondArray = {3, 4, 5, 6, 7};
-                    thirdArray = {5, 6, 7, 8, 9};
+        String[] arrayOfNames = {"Nikita", "John", "Mike", "Anthony", "Mary", "Ann"};
 
 
-                    for (int numberOfFirstArray : firstArray) {
-                        for (int numberOfSecondArray : secondArray) {
-                            for (int numberOfThirdArray : thirdArray) {
-                                if (numberOfFirstArray == numberOfSecondArray && numberOfSecondArray == numberOfThirdArray) {
-                                    System.out.println(numberOfFirstArray);
-                                }
-                            }
+        for (int i = 0; i < arrayOfNames.length; i++) {
+            System.out.println(arrayOfNames[i]);
+        }
+
+        // for each (will print all elements)
+        for (String name : arrayOfNames) {
+            System.out.println(name);
+
+        }
+
+        String[] countries = {
+                "United States",
+                "Canada",
+                "United Kingdom",
+                "Germany",
+                "France",
+                "Australia",
+                "Japan",
+                "India",
+                "China",
+                "Brazil"
+        };
+
+        for (int i = 0; i < countries.length; i++) {
+            if (countries[i].equals("Japan")) {
+                System.out.println("JAMETE KUDASAI!");
+                break;
+            }
+
+            int[] firstArray = {1, 2, 3, 4, 5};
+            int[] secondArray = {3, 4, 5, 6, 7};
+            int[] thirdArray = {5, 6, 7, 8, 9};
+
+            for (int numberOfFirstArray : firstArray) {
+                for (int numberOfSecondArray : secondArray) {
+                    for (int numberOfThirdArray : thirdArray) {
+                        if (numberOfFirstArray == numberOfSecondArray && numberOfSecondArray == numberOfThirdArray) {
+                            System.out.println(numberOfFirstArray);
                         }
                     }
                 }
             }
+
+
         }
+        for (int i = 0; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " ");
+            }
 
+            int x = 1;
+            int summa = 0;
+            while (x <= 5) {
+                summa = summa + x;
+                x++;
+            }
+            System.out.println("Summa" + summa);
+
+
+            //Print even numbers from 0 to 20
+            int y = 0;
+            while (y <= 20) {
+                System.out.println(y + " ");
+                y = y + 2;
+            }
+
+            int z = 1;
+            do {
+                System.out.println(z);
+                z++;
+            } while (z <= 5);
+
+
+        }
     }
-    do
-
-    {
-        System.out.println(z);
-        z++;
-    } while(z <=5);
-
-
 }
